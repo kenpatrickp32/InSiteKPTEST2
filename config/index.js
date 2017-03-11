4b8c3ee34c92dc3d1eac50e07de9732ca26ec4f5
Ken Peralta <kenperalta32@gmail.com>
2017-03-11 14:53:28 -0600

    added VCAP Service Credentials

---
+++ config/index.js
@@ -0,0 +1,9 @@
+//This will be the configuration index for the app that will take in VCAP service
+//credentials from JSON
+
+'use strict';
+
+const nconf = require('nconf');
+const path =require('path');
+
+nconf.argv();
